47549f7165882a1b5e5ec4b8f96f4beb9f16cece
Pietro Gallina <pietrogallina96@gmail.com>
2023-01-09 00:03:48 +0100

    Add DrinkController class with getAllDrinks, getDrinkById, getAllDrinksByView methods

--- src/main/java/it/team1Restaurant/application/Main.java
+++ src/main/java/it/team1Restaurant/application/Main.java
@@ -1,6 +1,7 @@
 package it.team1Restaurant.application;
 
 import it.team1Restaurant.controller.DishController;
+import it.team1Restaurant.controller.DrinkController;
 
 import static spark.Spark.*;
 
@@ -13,9 +14,17 @@ public class Main {
 
         System.out.printf("Server Jetty is running on localhost:%s\n\n", PORT);
 
+        //dishes REST API
+
         get("/restaurant/dishes", (req, res) -> DishController.getAllDishes(req, res));
         get("/restaurant/dishes/:id", (req, res) -> DishController.getDishById(req, res));
         get("/restaurant/dishes/view/:type_course", (req, res) -> DishController.getAllDishesByView(req, res));
+
+        //drinks REST API
+
+        get("/restaurant/drinks", (req, res) -> DrinkController.getAllDrinks(req, res));
+        get("/restaurant/drinks/:id", (req, res) -> DrinkController.getDrinkById(req, res));
+        get("/restaurant/drinks/view/:type_drink", (req, res) -> DrinkController.getAllDrinksByView(req, res));
 
     }
 
---
+++ src/main/java/it/team1Restaurant/controller/DrinkController.java
@@ -0,0 +1,37 @@
+package it.team1Restaurant.controller;
+
+import com.google.gson.GsonBuilder;
+import it.team1Restaurant.dao.ServiceDAOFactory;
+import it.team1Restaurant.foods.Dish;
+import it.team1Restaurant.foods.Drink;
+import it.team1Restaurant.foods.TypeCourseEnum;
+import it.team1Restaurant.foods.TypeDrinkEnum;
+import it.team1Restaurant.service.DrinkService;
+import spark.Request;
+import spark.Response;
+
+import java.util.List;
+
+public class DrinkController {
+
+    private static DrinkService drinkService = ServiceDAOFactory.getDrinkService();
+
+    public static String getAllDrinksByView(Request request, Response response){
+        String typeDrink = request.params(":type_drink");
+        TypeDrinkEnum typeDrinkEnum = TypeDrinkEnum.getTypeDrinkByName(typeDrink);
+        List<Drink> drinkList = drinkService.selectAllDrinksByView(typeDrinkEnum);
+        return new GsonBuilder().setPrettyPrinting().create().toJson(drinkList);
+    }
+
+    public static String getAllDrinks(Request request, Response response){
+        List<Drink> drinkList = drinkService.selectAllDrinks();
+        return new GsonBuilder().setPrettyPrinting().create().toJson(drinkList);
+    }
+
+    public static String getDrinkById(Request request, Response response){
+        Integer id = Integer.parseInt(request.params(":id"));
+        Drink drink = drinkService.selectDrinkById(id);
+        return new GsonBuilder().setPrettyPrinting().create().toJson(drink);
+    }
+
+}
