9a9335acf84d932ffbaba2bcf9321d0e127cd10e
Pietro Gallina <pietrogallina96@gmail.com>
2023-01-19 13:51:12 +0100

    Add IBookingDAO interface; Add BookingService class; Add getBookingService method in ServiceDAOFactory class; handle DataAccessException propagation in Booking'service, test classes

--- src/main/java/it/team1Restaurant/dao/ServiceDAOFactory.java
+++ src/main/java/it/team1Restaurant/dao/ServiceDAOFactory.java
@@ -1,5 +1,6 @@
 package it.team1Restaurant.dao;
 
+import it.team1Restaurant.service.BookingService;
 import it.team1Restaurant.service.DishService;
 
 public class ServiceDAOFactory {
@@ -7,6 +8,11 @@ public class ServiceDAOFactory {
     public static DishService getDishService(){
         DishDAO dishDAO = new DishDAO();
         return new DishService(dishDAO);
+    }
+
+    public static BookingService getBookingService(){
+        BookingDAO bookingDAO = new BookingDAO();
+        return new BookingService(bookingDAO);
     }
 
 }
--- src/main/java/it/team1Restaurant/dao/testDAO/TestBookingDAO.java
+++ src/main/java/it/team1Restaurant/dao/testDAO/TestBookingDAO.java
@@ -1,8 +1,10 @@
 package it.team1Restaurant.dao.testDAO;
 
 import it.team1Restaurant.bookings.Booking;
-import it.team1Restaurant.dao.BookingDAO;
 import it.team1Restaurant.dao.ClientDAO;
+import it.team1Restaurant.dao.ServiceDAOFactory;
+import it.team1Restaurant.exception.DataAccessException;
+import it.team1Restaurant.service.BookingService;
 import it.team1Restaurant.user.Client;
 
 import java.time.LocalDate;
@@ -14,48 +16,54 @@ public class TestBookingDAO {
 
     public static void main(String[] args) {
 
-        BookingDAO bookingDAO = new BookingDAO();
-        ClientDAO clientDAO = new ClientDAO();
+        try {
 
-        bookingDAO.createTable();
+            BookingService bookingService = ServiceDAOFactory.getBookingService();
+            ClientDAO clientDAO = new ClientDAO();
 
-        Client client1 = clientDAO.selectClientById(1);
-        Client client2 = clientDAO.selectClientById(2);
+            bookingService.createTable();
 
-        Booking booking1 = Client.book(client1, new ArrayList<>(List.of(client1)), LocalDate.of(2023, 1, 10), LocalTime.of(12, 30));
-        Booking booking2 = Client.book(client1, new ArrayList<>(List.of(client1)), LocalDate.of(2023, 1, 12), LocalTime.of(20, 0));
-        Booking booking3 = Client.book(client2, new ArrayList<>(List.of(client2)), LocalDate.of(2023, 1, 12), LocalTime.of(13, 30));
-        Booking booking4 = Client.book(client2, new ArrayList<>(List.of(client2)), LocalDate.of(2023, 2, 14), LocalTime.of(21, 0));
+            Client client1 = clientDAO.selectClientById(1);
+            Client client2 = clientDAO.selectClientById(2);
 
-        bookingDAO.insertBooking(booking1);
-        bookingDAO.insertBooking(booking2);
-        bookingDAO.insertBooking(booking3);
-        bookingDAO.insertBooking(booking4);
+            Booking booking1 = Client.book(client1, new ArrayList<>(List.of(client1)), LocalDate.of(2023, 1, 10), LocalTime.of(12, 30));
+            Booking booking2 = Client.book(client1, new ArrayList<>(List.of(client1)), LocalDate.of(2023, 1, 12), LocalTime.of(20, 0));
+            Booking booking3 = Client.book(client2, new ArrayList<>(List.of(client2)), LocalDate.of(2023, 1, 12), LocalTime.of(13, 30));
+            Booking booking4 = Client.book(client2, new ArrayList<>(List.of(client2)), LocalDate.of(2023, 2, 14), LocalTime.of(21, 0));
 
-        System.out.println("selectAllBookings\n");
-        List<Booking> bookingList = bookingDAO.selectAllBookings();
-        bookingList.forEach(Booking::printDetails);
+            bookingService.insertBooking(booking1);
+            bookingService.insertBooking(booking2);
+            bookingService.insertBooking(booking3);
+            bookingService.insertBooking(booking4);
 
-        System.out.println("selectBookingById\n");
-        Booking booking = bookingDAO.selectBookingById(4);
-        if(booking != null){
-            booking.printDetails();
+            System.out.println("selectAllBookings\n");
+            List<Booking> bookingList = bookingService.selectAllBookings();
+            bookingList.forEach(Booking::printDetails);
+
+            System.out.println("selectBookingById\n");
+            Booking booking = bookingService.selectBookingById(4);
+            if(booking != null){
+                booking.printDetails();
+            }
+            else {
+                System.out.println("No booking found");
+            }
+
+            System.out.println("selectBookingsByClient\n");
+            bookingList = bookingService.selectBookingsByClient(client2);
+            bookingList.forEach(Booking::printDetails);
+
+            System.out.println("selectBookingsByDate\n");
+            bookingList = bookingService.selectBookingsByDate(LocalDate.of(2023,1,12));
+            bookingList.forEach(Booking::printDetails);
+
+            System.out.println("selectBookingsByInterval\n");
+            bookingList = bookingService.selectBookingsByInterval(LocalDate.of(2023,1,1), LocalDate.of(2023, 1, 31));
+            bookingList.forEach(Booking::printDetails);
+
+        } catch (DataAccessException e) {
+            System.out.println(e.getMessage());
         }
-        else {
-            System.out.println("No booking found");
-        }
-
-        System.out.println("selectBookingsByClient\n");
-        bookingList = bookingDAO.selectBookingsByClient(client2);
-        bookingList.forEach(Booking::printDetails);
-
-        System.out.println("selectBookingsByDate\n");
-        bookingList = bookingDAO.selectBookingsByDate(LocalDate.of(2023,1,12));
-        bookingList.forEach(Booking::printDetails);
-
-        System.out.println("selectBookingsByInterval\n");
-        bookingList = bookingDAO.selectBookingsByInterval(LocalDate.of(2023,1,1), LocalDate.of(2023, 1, 31));
-        bookingList.forEach(Booking::printDetails);
 
     }
 
